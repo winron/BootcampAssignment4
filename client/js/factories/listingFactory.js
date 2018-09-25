6efd309872417cf0a9f19af49280e436718a61e7
Winton Liu <33133071+winron@users.noreply.github.com>
2018-09-25 15:57:33 -0400

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:8080/api/listings');
+      return $http.get('https://wintonsbootcamp6.herokuapp.com/api/listings');
     },
 
 	create: function(listing) {
-	  return $http.post('http://localhost:8080/api/listings', listing);
+	  return $http.post('https://wintonsbootcamp6.herokuapp.com/api/listings', listing);
     },
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-       return $http.delete('http://localhost:8080/api/listings/'+id);
+       return $http.delete('https://wintonsbootcamp6.herokuapp.com/api/listings/'+id);
     }
   };
 
